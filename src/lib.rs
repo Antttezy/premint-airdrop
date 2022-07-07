98b7a542107ac52d188f0aaf27d8647655a872c7
anton <antkumachev@gmail.com>
2022-07-07 12:43:22 +0300

    Update program id

--- src/lib.rs
+++ src/lib.rs
@@ -8,4 +8,4 @@ pub mod util;
 #[cfg(not(feature = "no-entrypoint"))]
 pub mod entrypoint;
 
-solana_program::declare_id!("AtQLjceKsgF99N6qYZCWgKr689neWd4285J7TrmuSHQe");
+solana_program::declare_id!("M1zWUwPXAoA77vQV4m7hGnZN9qtWM4BYZrA63EzBiyA");
