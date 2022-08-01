0963637648ce45034ac8ff72097704aa03cf10e3
Anton <antkumachev@gmail.com>
2022-08-01 11:57:20 +0300

    New ID

--- src/lib.rs
+++ src/lib.rs
@@ -8,4 +8,4 @@ pub mod util;
 #[cfg(not(feature = "no-entrypoint"))]
 pub mod entrypoint;
 
-solana_program::declare_id!("M1zWUwPXAoA77vQV4m7hGnZN9qtWM4BYZrA63EzBiyA");
+solana_program::declare_id!("9fHdkidrwJJamCk2EmFb45MzUoy4scoGRz1XJqqGZmGN");
